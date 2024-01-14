918688c2ea8c49c2c2c3f2fba35655119772e600
Adavi Lalith <adavilalith11@gmail.com>
2024-01-14 12:45:53 +0530

    adding links to navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <>
-      <Navbar title="Conway's Game Of Life"></Navbar>
+      <Navbar title="Conway's Game Of Life"
+        section1="Rules" link1="./app.js"
+        section2="Wiki" link2="https://en.wikipedia.org/wiki/Conway's_Game_of_Life"
+        section3="Source Code" link3="https://github.com/adavilalith/conways_game_of_life"
+        contactLink="https://www.linkedin.com/in/lalith-adavi-284378274/  "
+      ></Navbar>
     <main>
       <Grid rows="70" cols="70" style={{float:'right'}}></Grid>
     </main>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Navbar.css'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div className='navdiv'>
       <h1 className='logo'>Conway's Game of Life</h1>
       <nav class="nav__links">
-        <li ><a href="./app.js">Rules</a></li>
-        <li ><a href="./app.js">Wiki</a></li>
-        <li ><a href="./app.js">Source Code</a></li>
+        <li ><a href={props.link1} target="_blank" rel="noreferrer">{props.section1}</a></li>
+        <li ><a href={props.link2} target="_blank" rel="noreferrer">{props.section2}</a></li>
+        <li ><a href={props.link3} target="_blank" rel="noreferrer">{props.section3}</a></li>
       </nav>
-      <a href="./apps.js"><button id='contactbtn'>Contact</button></a>
+      <a href={props.contactLink} target="_blank" rel="noreferrer"><button id='contactbtn'>Let's Connect!</button></a>
     </div>
   )
 }
