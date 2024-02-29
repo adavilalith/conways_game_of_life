8277e7ac41ec67447b86c8c83352af316e931f18
Adavi Lalith <adavilalith11@gmail.com>
2024-02-29 14:37:38 +0530

    finishing the rules modal with images and embedded videos

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -27,27 +27,59 @@ function RulesModal(props) {
               </Row>
               <Row>
                 <Col className='col-9 h4'>Rules</Col>
-                <Col className='col-3 text-center'>Example</Col>
+                <Col className='col-3 text-center h4'>Examples</Col>
               </Row>
               <Row>
                 <Col><strong>For cells with life:</strong></Col>
               </Row>
-              <Row>
-               <Col>Underpopluation, If the alive cell has only one alive neighbours it <strong>dies</strong></Col>
+              <Row className='my-3'>
+               <Col className='col-9'>Underpopluation, If the alive cell has only one alive neighbours it <strong>dies</strong></Col>
+               <Col className='d-flex align-items-center justify-content-center'>
+                  <img src="./images/rule1_1.png" alt="" />
+                  <img className="mx-2" src="./images/right-arrow.png" alt=""  width={25}/>
+                  <img src="./images/rule1_2.png" alt="" />
+              </Col>
               </Row>
-              <Row>
-                <Col>Overpopluation, If the alive cell has more than 3 alive neighbours it <strong>dies</strong></Col>
+              <Row className="my-3">
+                <Col className="col-9">Overpopluation, If the alive cell has more than 3 alive neighbours it <strong>dies</strong></Col>
+                <Col className='d-flex align-items-center justify-content-center'>
+                  <img src="./images/rule2_1.png" alt="" />
+                  <img className="mx-2" src="./images/right-arrow.png" alt=""  width={25}/>
+                  <img src="./images/rule2_2.png" alt="" />
+              </Col>
               </Row>
-              <Row>
-                <Col>Stability, If the alive cell has 2 or 3 neighbours it continues to <strong>live  </strong> </Col>
+              <Row className="my-3">
+                <Col className="col-9">Stability, If the alive cell has 2 or 3 neighbours it continues to <strong>live  </strong> </Col>
+                <Col className='d-flex align-items-center justify-content-center'>
+                  <img src="./images/rule3_1.png" alt="" />
+                  <img className="mx-2" src="./images/right-arrow.png" alt=""  width={25}/>
+                  <img src="./images/rule3_2.png" alt="" />
+              </Col>
               </Row>
-              <Row>
+              <Row className="my-3">
                 <Col><strong>For dead cells:</strong></Col>
               </Row>
               <Row>
-                <Col>Population, If a dead cell has exactly 3 alive neighbours if becomes <strong>alive</strong></Col>
+                <Col className="col-9">Population, If a dead cell has exactly 3 alive neighbours if becomes <strong>alive</strong></Col>
+                <Col className='d-flex align-items-center justify-content-center'>
+                  <img src="./images/rule4_1.png" alt="" />
+                  <img className="mx-2" src="./images/right-arrow.png" alt=""  width={25}/>
+                  <img src="./images/rule4_2.png" alt="" />
+              </Col>
               </Row>
-
+              <Row>
+                <Col>
+                  <p className='h4 mt-5 mb-3'>What to learn more?</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col className='d-flex justify-content-center'>
+                <iframe src="https://www.youtube.com/embed/Kk2MH9O4pXY?si=9dUitGtXB7rNoyIG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </Col>
+                <Col className='d-flex justify-content-center'>
+                <iframe src="https://www.youtube.com/embed/R9Plq-D1gEk?si=y2-Uu5xQCsshBRS0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </Col>
+              </Row>
             </Container>
         </Modal.Body>
         <Modal.Footer>
